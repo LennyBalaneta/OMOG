c202885b423e832281b6fbdefa307d30d76d7de6
Marcos Balatka <m.balatka5@gmail.com>
2018-05-25 21:02:45 -0300

    BSpline quase

--- BSpline.js
+++ BSpline.js
@@ -2,7 +2,8 @@ BSpline.prototype = new CurveI();
 function BSpline(cps) {
   this.cPoints = cps;
   this.k = 4;
-  this.n = 6;
+  this.n = this.cPoints.length - 1;
+
   this.parameters = function() {
     return null;
   }
@@ -10,92 +11,69 @@ function BSpline(cps) {
   this.evaluate = function(x) {
     return null;
   }
-  
+
   this.showCurve = function() {
     strokeWeight(2);
-    for(var u=0 ; u<=this.n-this.k+2 ; u+=0.005) {
+    for (var u=0; u<=this.n-this.k+2; u+=0.005) {
       //stroke(cores[int(u)]);
       var x = this.getX(u);
       var y = this.getY(u);
-      //var cor = cores[this.getT(u)];
-      //stroke(cor[0], cor[1], cor[2]);
-      //print("u->" + u+ " | t(u)->"+this.getT(u));
       point(this.getX(u), this.getY(u));
-      //if(isNaN(x) || isNaN(y)) {
-      //}else {
-      //  point(this.getX(u), this.getY(u));
-      //}
     }
   }
-  
-  this.getT = function(i) {
-    //return the t[i] value for non-uniform B-Spline. Uniform B-spline uses knot vector
-    if(i < this.k) {
-      return 0;
-    }else if(i <= this.n) {
-      return i - this.k + 1;
-    }else {
-      return this.n - this.k + 2;
+
+  this.generateT = function() {
+    var uniform = false;
+    var ts = [];
+
+    if (uniform) {
+      var m = this.n-this.k+2;
+      var tot = this.n+this.k+1;
+      for (var i=0; i<=this.n+this.k+1; i++) {
+        ts.push((m/tot)*i);
+      }
+    } else {
+      for (var i=0; i<=this.n+this.k+1; i++) {
+        if (i < this.k) {
+          ts.push(0);
+        } else if (i <= this.n) {
+          ts.push(i - this.k + 1);
+        } else {
+          ts.push(this.n - this.k + 2);
+        }
+      }
     }
+    print(ts);
+    return ts;
   }
-  
+  this.t = this.generateT();
+
   this.getN = function(i, j, u) {
-    if(j == 0) {//sei la 0 ou 1
-      if(this.getT(i) <= u && u < this.getT(i+1)) {
+    if (j == 0) {//sei la 0 ou 1
+      if (this.t[i] <= u && u <= this.t[i+1]) {
         return 1;
-      }else {
+      } else {
         return 0;
       }
     }
-    
+
     //k > 1
-    //print("wtf:"+this.l);
-    //var nikuOld = ((u-this.getT(i))*(this.getN(i, k-1, u)))/(this.getT(i+this.l-1) - this.getT(i)) + ((this.getT(i+this.k)-u)*this.getN(i+1, k-1, u))/(this.getT(i+this.k)-this.getT(i+1));
-    //var niku = ((u-this.getT(i))*(this.getN(i, k-1, u)))/(this.getT(i+k-1) - this.getT(i)) + ((this.getT(i+k)-u)*this.getN(i+1, k-1, u))/(this.getT(i+k)-this.getT(i+1));
-    //print("niku: "+niku);
-    var t1 = ((u-this.getT(i))) * this.getN(i, j-1, u);
-    var dt1 = (this.getT(i+j)-this.getT(i));
-    if(dt1 == 0) {
+    var t1 = ((u-this.t[i])) * this.getN(i, j-1, u);
+    var dt1 = (this.t[i+j]-this.t[i]);
+    if (dt1 == 0) {
       dt1 = 1;
       t1 = 0;
     }
-    var t2 = ((this.getT(i+j+1) - u)) * this.getN(i+1, j-1, u);
-    var dt2 = (this.getT(i+j+1) - this.getT(i+1));
-    if(dt2 == 0) {
+    var t2 = ((this.t[i+j+1] - u)) * this.getN(i+1, j-1, u);
+    var dt2 = (this.t[i+j+1] - this.t[i+1]);
+    if (dt2 == 0) {
       dt2 = 1;
       t2 = 0;
     }
     var niku = t1/dt1 + t2/dt2;
-    
-    
-    //v3
-    //var t1 = (u - this.getT(i)) * this.getN(i, j-1, u);
-    //var dt1 = (this.getT(i+j) - this.getT(i));
-    //var t2 = (this.getT(i+j+1) - u) * this.getN(i+1, j-1, u);
-    //var dt2 = (this.getT(i+j+1) - this.getT(i+1));
-    //if(dt1 == 0) {
-    //  dt1 = 1;
-    //  t1 = 0;
-    //}
-    //if(dt2 == 0) {
-    //  dt2 = 1;
-    //  t2 = 0;
-    //}
-    
-    //v2
-    //var t1 = (u-this.getT(i))*this.getN(i, j-1, u);
-    //var dt1 = this.getT(i+j-1) - this.getT(i);
-    //var t2 = (this.getT(i+j) - u)*this.getN(i+1, j-1, u);
-    //var dt2 = this.getT(i+j) - this.getT(i+1);
-    var niku = t1/dt1 + t2/dt2;
-    if(isNaN(niku)) {
-      //print("t1="+t1+" | dt1="+dt1+" | t2="+t2+" | dt2="+dt2);
-      //print("i:"+i+" | k:"+j+" | u:"+i);
-      //return 0;
-    }
     return niku;
   }
-  
+
   this.getX = function(u) {
     var valX = 0;
     for (var i=0; i<this.cPoints.length; i++) {
@@ -103,13 +81,15 @@ function BSpline(cps) {
     }
     return valX;
   }
-  
+
   this.getY = function(u) {
     var valY = 0;
+    //print("u: "+u);
     for (var i=0; i<this.cPoints.length; i++) {
+      //print("i:"+i+" | cPoints[i]:"+cPoints[i].y+" | "+this.getN(i, this.k, u));
       valY += this.getN(i, this.k, u) * cPoints[i].y;
     }
+    //print("valY: "+valY);
     return valY;
   }
-  
 }
--- omog.js
+++ omog.js
@@ -4,11 +4,11 @@ var cores;
 
 function setup() {
   cores = [];
-  for(var i=0 ; i<20 ; i++) {
+  for (var i=0; i<20; i++) {
     var cor = [random(0, 255), random(0, 255), random(0, 255)]
-    cores.push(cor);
+      cores.push(cor);
   }
-  
+
   createCanvas(800, 600);
   frameRate(30);
   //array of curves
